2bbd6019a7dfac8d9a798df1c3b78f46a1cba00c
kendraash <kendra.ash32@gmail.com>
2017-07-20 08:13:28 -0700

    adding styling and making messages an object

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ require('dotenv').config()
 const app = express()
 const port = 3000
 
-const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID
-const AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
+const ACCOUNT_SID = 'AC623bc71d4f94223602f23580f5f3e2c3'
+const AUTH_TOKEN = '1178a4bc9924c486f57f2a553c425a42'
 const client = new Twilio(ACCOUNT_SID, AUTH_TOKEN)
 
 // App config
@@ -20,8 +20,10 @@ app.use(express.static('assets'))
 // Top level route
 app.get('/', async (request, response) => {
   const twilioMessages = await client.messages.list()
-  console.log(twilioMessages);
-  const messageBodies = twilioMessages.map(fullMessage => fullMessage.body)
+  console.log(twilioMessages)
+  const messageBodies = twilioMessages.filter(fullMessage => fullMessage.direction === 'inbound').map(fullMessage => {
+    return {body: fullMessage.body, from: fullMessage.from, to: fullMessage.to}
+  })
   response.render('view', {messages: messageBodies})
 })
 
